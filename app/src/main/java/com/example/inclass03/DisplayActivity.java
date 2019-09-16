0722a2dc29f6a8aef650a6ed693b9b0655654632
sgupta180496 <53239577+sgupta180496@users.noreply.github.com>
2019-09-16 12:48:04 -0400

    Update DisplayActivity.java

--- app/src/main/java/com/example/inclass03/DisplayActivity.java
+++ app/src/main/java/com/example/inclass03/DisplayActivity.java
@@ -1,10 +1,3 @@
-/*
-   InClass 03: My Profile
-   Group #: 28
-   Saloni Gupta 801080992
-   Renju Hanna Robin 801076715
-*/
-
 package com.example.inclass03;
 
 import android.content.Intent;
@@ -14,7 +7,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.annotation.Nullable;
-//import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.TextView;
 
@@ -28,25 +20,13 @@ public class DisplayActivity extends AppCompatActivity {
     final public static String BUNDLETOEDIT = "BUNDLETOEDIT";
     final public static String TOEDIT = "TOEDIT";
 
-
-
-
     final int REQ_CODE = 200;
 
-    /**
-     * Dispatch incoming result to the correct fragment.
-     *
-     * @param requestCode
-     * @param resultCode
-     * @param data
-     */
-    //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == REQ_CODE) {
             if (resultCode == RESULT_OK &&  data!= null) {
-
 
                 final Bundle extraFromMain = data.getExtras().getBundle(MainActivity.TAGIMAGE);
                 final User user = (User)extraFromMain.getSerializable(USERTODISPLAY);
@@ -62,16 +42,10 @@ public class DisplayActivity extends AppCompatActivity {
                     }
 
                 }
-
-
-
-
             }
         }
-
     }
 
-    //@RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -88,7 +62,6 @@ public class DisplayActivity extends AppCompatActivity {
         if (user != null) {
             if(user.getGender().equals(MainActivity.MALE)) {
                 displayImage.setImageDrawable(getDrawable(R.drawable.male));
-
             } else {
                 displayImage.setImageDrawable(getDrawable(R.drawable.female));
             }
@@ -104,10 +77,8 @@ public class DisplayActivity extends AppCompatActivity {
                     Bundle sentData = new Bundle();
                     sentData.putSerializable(BUNDLETOEDIT, user);
                     toEdit.putExtra(TOEDIT,sentData);
-
                     startActivityForResult(toEdit,REQ_CODE);
                 }
-            });
-
+          });
     }
 }
